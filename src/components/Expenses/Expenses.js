506dd7665956f24fcd02bcfe62855c13b0498c52
aamonterroso <allan.monterroso1@gmail.com>
2022-09-29 03:19:45 -0600

    organized overall app structure and modularized

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem'
+import Card from '../UI/Card'
+import './Expenses.css'
+
+const Expenses = (props) => {
+
+  return (
+    <Card className="expenses">
+      <ExpenseItem item={props.items[0]}></ExpenseItem>
+      <ExpenseItem item={props.items[1]}></ExpenseItem>
+      <ExpenseItem item={props.items[2]}></ExpenseItem>
+      <ExpenseItem item={props.items[3]}></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
